87ffec83c25060bf0274475b3e016a0a621c9c40
Serina Priyadarshini Barik S <serinapriyadarshini3355@gmail.com>
2026-08-07 15:16:22 +0530

    Day 3: Performance Analytics completed

--- bluestock_mf_capstone/scripts/data_cleaning.py
+++ bluestock_mf_capstone/scripts/data_cleaning.py
@@ -106,3 +106,18 @@ perf.to_csv(
 )
 
 print("scheme_performance_cleaned.csv saved successfully!")
+files = [
+    "01_fund_master.csv",
+    "03_aum_by_fund_house.csv",
+    "04_monthly_sip_inflows.csv",
+    "05_category_inflows.csv",
+    "06_industry_folio_count.csv",
+    "09_portfolio_holdings.csv",
+    "10_benchmark_indices.csv"
+]
+
+for file in files:
+    df = pd.read_csv(RAW_DIR / file)
+    df.to_csv(PROCESSED_DIR / file, index=False)
+
+print("Remaining datasets copied successfully!")
